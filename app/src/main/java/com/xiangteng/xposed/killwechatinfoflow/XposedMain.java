893e750d9f6c47d91734464d6a663a1706ffa52e
xiangteng <xiangtengpku@gmail.com>
2020-03-18 13:55:30 +0800

    在 hook 的同时尝试将配置写回微信的配置文件，理论上成功一次后关闭模块也能继续生效

--- app/src/main/java/com/xiangteng/xposed/killwechatinfoflow/XposedMain.java
+++ app/src/main/java/com/xiangteng/xposed/killwechatinfoflow/XposedMain.java
@@ -9,6 +9,7 @@ import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
@@ -19,96 +20,42 @@ public class XposedMain implements IXposedHookLoadPackage {
     private static ClassLoader classloader;
 
     @Override
-    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) {
         if (!lpparam.packageName.equals("com.tencent.mm"))
+            return;
+        if (!lpparam.processName.equals("com.tencent.mm"))
             return;
         Log.i(LOG_TAG, "WeChat Hooked.");
         try {
             findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                protected void afterHookedMethod(MethodHookParam param) {
                     Log.i(LOG_TAG, "attach() hooked.");
                     classloader = ((Context) param.args[0]).getClassLoader();
                     hook();
                 }
             });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook attach() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
+        } catch (Throwable t) {
+            Log.e(LOG_TAG, "Hook attach() error.", t);
         }
     }
 
     private void hook() {
-        /*try {
-            findAndHookMethod("com.tencent.mm.storage.s", classloader, "blj", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Log.i(LOG_TAG, "com.tencent.mm.storage.s.blj() called.");
-                    param.setResult(false);
-                }
-            });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook com.tencent.mm.storage.s.blj() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
-        }
-
         try {
-            findAndHookMethod("com.tencent.mm.storage.s", classloader, "bvn", new XC_MethodHook() {
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    Log.i(LOG_TAG, "com.tencent.mm.storage.s.bvn() called.");
-                    param.setResult(false);
-                }
-            });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook com.tencent.mm.storage.s.bvn() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
-        }
-
-        try {
-            findAndHookMethod("com.tencent.mm.sdk.platformtools.az", classloader, "decodeInt", String.class, int.class, new XC_MethodHook() {
+            findAndHookMethod("com.tencent.mmkv.MMKV", classloader, "decodeInt", long.class, String.class, int.class, new XC_MethodHook() {
                 @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    if (param.args[0].toString().equals("BizTimeLineOpenStatus")) {
-                        Log.i(LOG_TAG, "com.tencent.mm.sdk.platformtools.az.decodeInt() called.");
-                        param.setResult(0);
-                    }
-                }
-            });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook com.tencent.mm.sdk.platformtools.az.decodeInt() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
-        }
-
-        try {
-            findAndHookMethod("com.tencent.mm.sdk.platformtools.as", classloader, "decodeInt", String.class, int.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    if (param.args[0].toString().equals("BizTimeLineOpenStatus")) {
-                        Log.i(LOG_TAG, "com.tencent.mm.sdk.platformtools.as.decodeInt() called.");
-                        param.setResult(0);
-                    }
-                }
-            });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook com.tencent.mm.sdk.platformtools.as.decodeInt() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
-        }*/
-
-        try {
-            findAndHookMethod("com.tencent.mmkv.MMKV", classloader, "decodeInt", String.class, int.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    if (param.args[0].toString().equals("BizTimeLineOpenStatus")) {
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    if (param.args[1].equals("BizTimeLineOpenStatus")) {
                         Log.i(LOG_TAG, "com.tencent.mmkv.MMKV.decodeInt(\"BizTimeLineOpenStatus\") called.");
                         param.setResult(0);
+                        XposedHelpers.callMethod(param.thisObject, "putInt", "BizTimeLineOpenStatus", 0);
                     }
                 }
             });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook com.tencent.mmkv.MMKV.decodeInt() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
+        } catch (Throwable t) {
+            Log.e(LOG_TAG, "Hook com.tencent.mmkv.MMKV.decodeInt() error.", t);
         }
 
-        try {
+        /*try {
             findAndHookMethod(Activity.class, "startActivity", Intent.class, Bundle.class, new XC_MethodHook() {
                 protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                     Intent intent = (Intent) param.args[0];
@@ -120,10 +67,9 @@ public class XposedMain implements IXposedHookLoadPackage {
                     }
                 }
             });
-        } catch (Throwable e) {
-            Log.e(LOG_TAG, "Hook startActivity() error.");
-            Log.e(LOG_TAG, Log.getStackTraceString(e));
-        }
+        } catch (Throwable t) {
+            Log.e(LOG_TAG, "Hook startActivity() error.", t);
+        }*/
     }
 }
 
